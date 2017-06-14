d78b1adc4d939ee35d2d40dfbc4d0dd117f4c57e
fran-huang <laplace55@gmail.com>
2017-06-14 17:17:29 +0800

    carbon-monoxide

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: fran
+ *
+ * Created on June 14, 2017, 5:14 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
